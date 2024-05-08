4180639e895a5afe170aaecc7eea1862e4afe5c2
zhenyujiang1 <145005523+zhenyujiang1@users.noreply.github.com>
2024-05-08 18:01:51 +0800

    Create sims22.py

---
+++ sims22.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-   编码声明
+
+
+import subprocess
+
+
+def main():
+
+    for i in [0.9,1.0]:
+        print(i)
+        with open('vb2_{}.out'.format(i), 'w') as f:
+            subprocess.call(['python', 'raser/field/bjt_circuit22.py', str(i)], stdout=f, stderr=subprocess.STDOUT)
+#subprocess.STDOUT 参数用于将标准错误输出合并到标准输出中
