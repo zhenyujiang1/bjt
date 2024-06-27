2379982d664760b8364dc54ba6c32e169d1cbe17
zhenyujiang1 <145005523+zhenyujiang1@users.noreply.github.com>
2024-06-27 11:41:33 +0800

    Update bjt_circuit22.py

--- bjt_circuit22.py
+++ bjt_circuit22.py
@@ -19,12 +19,11 @@ from raser.field import bjt_common22
 bjt_common22.run()##设置物理参数，求解, load bjt_dd_0.msh
 
 from ramp2 import *
-rampvoltage("bjt", "Vb", 2.5, Vb, 0.01, 0.0001, 100, 1e-2, 1e6, bjt_common22.make_bias("base"))
-rampvoltage("bjt", "Vc", 2.5, 60, 0.005, 0.0001, 100, 1e-2, 1e6, bjt_common22.make_sweep(("base", "collector", "emitter"), ("Vb", "Vc", "Ve")))
-##迭代的细节
+#rampvoltage("bjt", "Vb", 2.5, Vb, 0.01, 0.0001, 100, 1e-2, 1e6, bjt_common22.make_bias("base"))
 
-#initial:
-#rampvoltage("bjt", "Vb", 0.0, Vb, 0.1, 0.001, 40, 1e-2, 1e6, bjt_common22.make_bias("base"))
-#rampvoltage("bjt", "Vc", 0.0, 1.5, 0.05, 0.0001, 10, 1e-3, 1e6, bjt_common22.make_sweep(("base", "collector", "emitter"), ("Vb", "Vc", "Ve")))
+#Vb=3V
+rampvoltage("bjt", "Vb", 2.5, Vb, 0.025, 0.0001, max_iter=30, rel_error=1e-14, abs_error=1e20, callback=bjt_common22.make_bias("base"))
+rampvoltage("bjt", "Vc", 0, 4, 0.025, 0.00001, 100, 1e-4, 1e20, bjt_common22.make_sweep(("base", "collector", "emitter"), ("Vb", "Vc", "Ve")))
 
-#def rampvoltage(device, Vsource, begin_bias, end_bias, init_step_size, min_step, max_iter, rel_error, abs_error, callback):
+
+#为了得到Ic-Vce的图像，即输出特性曲线，Vb固定
